e620dcaa04b691681799fabe287a9f2adf1fb9ff
kingcuervo <joseramirezmaracaibo@gmail.com>
2017-03-09 12:12:44 -0400

    Add files via upload

---
+++ domain_checker.php
@@ -0,0 +1,4 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/Core/Neutron.Core.php");
+Template::get(domain_checker);
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+﻿<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/Core/Neutron.Core.php");
+Template::get(index);
+?>
